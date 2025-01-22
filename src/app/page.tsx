dbabb3c77b7a5c7bd687bc2b6bd0dd39a9c0446f
Coders World <161362136+Aahilmalik85@users.noreply.github.com>
2025-01-22 21:49:48 +0500

    second commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import BeautifulRoom from "@/Components/beautiFullRoom/BeautifullRoom";
+import Browserange from  "@/Components/browser-range/Browser-range"
+import Hero from "@/Components/hero/Hero";
+import OurProducts from "@/Components/our-products/Product";
+import PhotoGallery from "@/Components/photogallery/Gallery";
+import SimpleForm from "@/Components/SimpleForm";
+
+
+export default function Home() {
+  return (
+    <>
+    <Hero/>
+    <Browserange/>
+
+     <OurProducts/>
+    <BeautifulRoom/>
+
+    <SimpleForm />
+
+    <PhotoGallery/>
+
+ 
+
+
+
+    z</>
+  );
+}
